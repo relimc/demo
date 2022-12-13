6f7ca6754b38a18574f4c09a4cc8409b2f263c3b
基础的魅力 <accounts_6124fb009b0b7a002d9f7697@mail.teambition.com>
2022-12-13 16:50:10 +0800

    update

--- android/theFirstCodeForAndroid/DatabaseTest/app/src/main/java/com/example/databasetest/MyDatabaseHelper.kt
+++ android/theFirstCodeForAndroid/DatabaseTest/app/src/main/java/com/example/databasetest/MyDatabaseHelper.kt
@@ -10,10 +10,19 @@ import android.widget.Toast
 // name 表示你要创建的数据库文件，version 表示数据库版本
 // SQLiteOpenHelper 是个抽象类，无法直接只用此类来创建和管理数据库，我们必须创建一个其子类。
 // 在这里，MyDatabaseHelper 继承了 SQLiteOpenHelper，并实现了 SQLiteOpenHelper 的两个抽象方法
-// onCreate 方法在实例化 MyDatabaseHelper 对象时执行，其接收一个 SQLiteDatabase 对象作为参数
-// SQLiteDatabase 是个管理 SQlite 数据库的工具类，可用于新建数据库表和对表数据的增删改查
+// onCreate 方法在 SQLiteOpenHelper 的 getReadableDatabase() 或者 getWritableDatabase() 被调用时执行，其接收一个 SQLiteDatabase 对象作为参数
+// 同时，getReadableDatabase() 或者 getWritableDatabase() 的返回值也是个 SQLiteDatabase 对象
+// SQLiteDatabase 是个管理 SQlite 数据库的工具类，可用于新建数据库表，可用于对数据的增删改查
 // 在 onCreate 方法中，我们使用 SQLiteDatabase 对象的 execSQL 命令，执行了两行建表命令
-// onUpgrade 方法同样会在创建 MyDatabaseHelper 对象时执行，不过其执行条件还包含一个，
+
+// onUpgrade 方法同样会在 SQLiteOpenHelper 的 getReadableDatabase() 或者 getWritableDatabase() 被调用时执行，不过 onUpgrade 方法的执行还受其他条件的约束
+// 在初始化一个 MyDatabaseHelper 对象时，只有指定的数据库（MyDatabaseHelper 构造器的第二个参数）不存在，才会触发这个方法
+// 而当指定的数据库存在时，需要传递与之前不一样 version 参数，才会触发这个方法
+// 当新 version 比旧 version 大时，会触发 onUpgrade 方法
+// 当新 version 比旧 version 小时，会触发 onDowngrade 方法
+// onUpgrade 方法的使用场景是，当我们安装了版本号为 1 的应用时，创建了数据库 A，数据库版本为 1。现在我们对数据库做了些改动，比如新增一张表，准备升级应用为版本 2。
+// 如果此时数据库版本仍然是1，我们安装了版本号为 2 的应用时，将无法完成数据库的升级操作，onUpgrade 方法将无法执行。
+// 只有将数据库版本改为2，才会触发 onUpgrade 方法。
 class MyDatabaseHelper(val contenxt: Context, name: String, version: Int): SQLiteOpenHelper(contenxt, name, null, version) {
 
     private val createBook = "create table Book (" +

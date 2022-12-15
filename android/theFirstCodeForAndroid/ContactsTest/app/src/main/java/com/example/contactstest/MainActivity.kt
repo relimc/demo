f2da1e519576880732f571e05998e756afbb6d71
基础的魅力 <accounts_6124fb009b0b7a002d9f7697@mail.teambition.com>
2022-12-15 11:29:30 +0800

    update

--- android/theFirstCodeForAndroid/ContactsTest/app/src/main/java/com/example/contactstest/MainActivity.kt
+++ android/theFirstCodeForAndroid/ContactsTest/app/src/main/java/com/example/contactstest/MainActivity.kt
@@ -31,12 +31,18 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun readContacts() {
+        // getContentResolver 方法用于获取 ContentResolver 对象，
+        // 这个对象可以对 ContentProvider 提供的数据进行增删改查的操作
+        // ContentProvider 是内容提供者，ContentResolver 是内容消费者
         contentResolver.query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI, null, null, null, null)?.apply {
             while (moveToNext()) {
                 val displayName = getString(getColumnIndex(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME))
                 val number = getString(getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER))
                 contactsList.add("$displayName\n$number")
             }
+            // while 循环后，adapter 的数据源 contactsList 发生了改变。
+            // 当数据源发生改变时，需要通知 ListView 展示更改后的数据源
+            // 通知的动作是由 notifyDataSetChanged 方法来完成的
             adapter.notifyDataSetChanged()
             close()
         }

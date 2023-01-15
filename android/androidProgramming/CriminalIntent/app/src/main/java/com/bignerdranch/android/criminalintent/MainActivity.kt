1feaf09f9577ebdd59eba1dcf5e491697fd2f715
relimc <relimc@outlook.com>
2023-01-16 00:04:51 +0800

    update

---
+++ android/androidProgramming/CriminalIntent/app/src/main/java/com/bignerdranch/android/criminalintent/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.bignerdranch.android.criminalintent
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val currentFragment = supportFragmentManager.findFragmentById(R.id.fragment_container)
+        if (currentFragment == null) {
+            val fragment = CrimeListFragment.newInstance()
+            supportFragmentManager
+                .beginTransaction()
+                .add(R.id.fragment_container, fragment)  // 将 fragment 添加到视图容器 fragment_container 中
+                .commit()
+        }
+    }
+}

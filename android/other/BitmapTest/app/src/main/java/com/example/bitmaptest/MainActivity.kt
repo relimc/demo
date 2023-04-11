da9bfff937d2162a975c31420e36ad78bb50120e
W9007465 <v-liujialiang@oppo.com>
2023-04-11 19:54:16 +0800

    update

--- android/other/BitmapTest/app/src/main/java/com/example/bitmaptest/MainActivity.kt
+++ android/other/BitmapTest/app/src/main/java/com/example/bitmaptest/MainActivity.kt
@@ -1,21 +1,24 @@
 package com.example.bitmaptest
 
+import android.content.Context
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.ImageView
+import kotlin.math.log
+
+private const val TAG = "MainActivity"
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val path = "C:\\Users\\liuji\\Pictures\\微信截图_20230405142320.png"
-        val options = BitmapFactory.Options()
-        options.inJustDecodeBounds = true
-        BitmapFactory.decodeFile(path, options)
-
-        val picWidth = options.outWidth
-        val picHeight = options.outHeight
+        val context = applicationContext
+        val externalFiles = context.getExternalFilesDir(null)
+        val externalCache = context.externalCacheDir
+        Log.d(TAG, externalCache.toString())
+        Log.d(TAG, externalFiles.toString())
     }
 }
